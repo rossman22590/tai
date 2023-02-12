ed1d5ad9344fbf838023c401d87d5ed8ac1fcb16
Katy Smith <113362369+catrionafsmith@users.noreply.github.com>
2023-02-12 23:32:07 +1100

    API key issue fixed

--- netlify/functions/get-token.js
+++ netlify/functions/get-token.js
@@ -1,7 +1,7 @@
 exports.handler = async function () {
     try {
         const subject = process.env.API_KEY
-        return { statusCode: 200, body: JSON.stringify({ message: `$subject`})}  
+        return { statusCode: 200, body: JSON.stringify({ message: subject})}  
     } catch (err) {
         return { statusCode: 500, body: err.toString() }
     }
